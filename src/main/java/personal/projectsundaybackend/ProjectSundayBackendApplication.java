d93b59219272f0d1794ec8e827613c064f5398ee
Leo Schaffner <leoschaffner935@gmail.com>
2022-08-11 17:32:02 -0400

    Added Spring Initializr code

---
+++ src/main/java/personal/projectsundaybackend/ProjectSundayBackendApplication.java
@@ -0,0 +1,13 @@
+package personal.projectsundaybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSundayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSundayBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/personal/projectsundaybackend/ProjectSundayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.projectsundaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSundayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
