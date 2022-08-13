d0bfa8e24b0184b1b29bd21740075103b15736fd
Leo Schaffner <leoschaffner935@gmail.com>
2022-08-13 14:29:26 -0400

    Added dependencies for jpa, postgresql, and logback. Imported javax.persistence into entity classes

--- src/main/java/personal/projectsundaybackend/entities/Post.java
+++ src/main/java/personal/projectsundaybackend/entities/Post.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
+import javax.persistence.*;
 
 import java.io.File;
 
